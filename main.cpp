f8107fbd68044e9b5a4a1a0e827842ba97120b0b
GabrieleTriscari <s312175@studenti.polito.it>
2025-05-16 15:45:51 +0200

    modifiche

--- main.cpp
+++ main.cpp
@@ -25,5 +25,33 @@ int main()
 	cout << "Dodecaedro" << endl;
 	cc.printFaces();
 	
+	/*//Per visualizzare online le mesh
+    Gedim::UCDUtilities utilities;
+    utilities.ExportPoints("./Cell0Ds.inp",
+                           mesh.cell0Ds_coordinates);
+
+    utilities.ExportSegments("./Cell1Ds.inp",
+                             mesh.cell0Ds_coordinates,
+                             mesh.cell1Ds_extrema);
+    */
+   Gedim::UCDUtilities utils;
+
+   
+
+    // 2. Estrai coordinate in una matrice
+	MatrixXd coords(3, t.vertices.size());
+    for (size_t i = 0; i < t.vertices.size(); ++i) {
+        coords(0, i) = t.vertices[i].x;
+        coords(1, i) = t.vertices[i].y;
+        coords(2, i) = t.vertices[i].z;
+    }
+
+    // 3. Costruisci la lista degli indici dei vertici per il solido (un tetraedro ha 4 vertici)
+    vector<vector<unsigned int>> celle = {
+        {0, 1, 2, 3}
+    };
+
+    // 4. Esporta il file per ParaView
+    utils.ExportPolyhedra("tetraedro.inp", coords, celle);
 	return 0;
 }
